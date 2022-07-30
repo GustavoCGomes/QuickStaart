3f9ea97191a35005fdd9a8d2f17628f1e5ac9d3d
Gustavo Gomes <108428536+GustavoCGomes@users.noreply.github.com>
2022-07-30 09:51:02 -0300

    Javascript

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
-console.log("Olá Gustavo")
+function revelarResposta(){
+    var resposta = document.querySelector("#resposta");
+    resposta.classList.toggle("blur");
+}
+
+function proximaPergunta(proximaPergunta) {
+    var pergunta = document.querySelector("#cardContainer");
+    pergunta.innerHTML = "";
+
+    var cardDiv = document.createElement("div")
+
+    cardDiv.classList.add("card")
+
+    cardDiv.innerHTML = `
+        <div class="card-cabecalho centralizar">
+            <h1 class="card-titulo">O que é ${proximaPergunta.title}?</h1>
+        </div>
+
+        <div id="resposta" class="card-conteudo blur">
+            <p>
+                ${proximaPergunta.description}
+            </p>
+        </div>
+    `;
+
+    pergunta.appendChild(cardDiv)
+}
+
+function buscarInformacao() {
+    fetch("https://flash.quickstaart.com/random")
+    .then(function(resultado) {
+        return resultado.json();
+    })
+    .then(function (resultadoJson) {
+        proximaPergunta(resultadoJson);
+    })
+}
+
+window.addEventListener("load", buscarInformacao);
